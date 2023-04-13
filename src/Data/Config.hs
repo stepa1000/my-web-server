160bb73156f4f2b00042a32ba243f3cf180a2d21
stepa <stepa100023@gmail.com>
2023-04-13 21:20:07 +0600

    removed the configuration example from the code.

--- src/Data/Config.hs
+++ src/Data/Config.hs
@@ -1,73 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Data.Config
-  ( defaultServerSettings,
-    getServerSettings,
-    testDefaultServerSettings,
+  ( getServerSettings,
     getServerSettingsTest,
     initNewsCategoryMain,
     initNewsCategoryTest,
   )
 where
 
-import qualified Control.Logger as Logger
 import qualified Data.Imp.Server as Server
-import qualified Data.Imp.Server.Authorization as Authorization
 import qualified Data.Imp.Server.Category as Category
-import qualified Data.Imp.Server.News as News
-import qualified Data.Logger.Impl as ImpLogger
 import Data.Yaml as Y
-import Database.Beam.Postgres as Beam
-
-defaultServerSettings :: IO ()
-defaultServerSettings = do
-  encodeFile "./config/server.yaml" $
-    Server.Config
-      { Server.confNews =
-          News.Config
-            { News.confMaxLimit = 10
-            },
-        Server.confFailPathToCategoryNews = "./data/categoryNews.json",
-        Server.confAuthorization =
-          Authorization.Config
-            { Authorization.confLimit = 10
-            },
-        Server.confConnectionInfo = mainDBConnect,
-        Server.confLogger =
-          ImpLogger.PreConfig
-            { ImpLogger.preconfFilePath = "./logging/log",
-              ImpLogger.preconfMinLevel = Logger.Error
-            }
-      }
-
-testDefaultServerSettings :: IO ()
-testDefaultServerSettings = do
-  encodeFile "./config/serverTest.yaml" $
-    Server.Config
-      { Server.confNews =
-          News.Config
-            { News.confMaxLimit = 10
-            },
-        Server.confFailPathToCategoryNews = "./data/categoryNewsTest.json",
-        Server.confAuthorization =
-          Authorization.Config
-            { Authorization.confLimit = 10
-            },
-        Server.confConnectionInfo = testDBConnect,
-        Server.confLogger =
-          ImpLogger.PreConfig
-            { ImpLogger.preconfFilePath = "./logging/logTest",
-              ImpLogger.preconfMinLevel = Logger.Debug
-            }
-      }
-
-mainDBConnect :: ConnectInfo
-mainDBConnect =
-  defaultConnectInfo {connectUser = "stepan", connectDatabase = "newsdb", connectPassword = "123"}
-
-testDBConnect :: ConnectInfo
-testDBConnect =
-  defaultConnectInfo {connectUser = "stepan", connectDatabase = "testdb", connectPassword = "123"}
 
 initNewsCategoryMain :: IO ()
 initNewsCategoryMain = do
